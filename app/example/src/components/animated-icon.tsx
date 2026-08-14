3c5cd88e09fa7ff4053e90e77ae1ce65773a6af5
Azimeh Nasara obadiah <azimehobadiah1@gmail.com>
2026-08-14 06:54:39 +0100

    reset project to empty state.

--- app/example/src/components/animated-icon.tsx
+++ app/example/src/components/animated-icon.tsx
@@ -33,7 +33,7 @@ export function AnimatedSplashOverlay() {
     },
   });
 
-  const image = <Image style={styles.image} source={require('@/assets/images/expo-logo.png')} />;
+  const image = <Image style={styles.image} source={require('@/assets/images/logo.png')} />;
 
   return animate ? (
     <Animated.View
@@ -99,12 +99,12 @@ export function AnimatedIcon() {
   return (
     <View style={styles.iconContainer}>
       <Animated.View entering={glowKeyframe.duration(60 * 1000 * 4)} style={styles.glow}>
-        <Image style={styles.glow} source={require('@/assets/images/logo-glow.png')} />
+        <Image style={styles.glow} source={require('@/assets/images/logo.png')} />
       </Animated.View>
 
       <Animated.View entering={keyframe.duration(DURATION)} style={styles.background} />
       <Animated.View style={styles.imageContainer} entering={logoKeyframe.duration(DURATION)}>
-        <Image style={styles.image} source={require('@/assets/images/expo-logo.png')} />
+        <Image style={styles.image} source={require('@/assets/images/logo.png')} />
       </Animated.View>
     </View>
   );
